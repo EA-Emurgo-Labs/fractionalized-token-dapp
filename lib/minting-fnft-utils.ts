7f0441d17c33538e8d4d45255a8654a69f5b6e24
thinhnguyen <thinhnq@blockchainlabs.asia>
2023-08-16 18:14:03 +0700

    can mint and burn

--- lib/minting-fnft-utils.ts
+++ lib/minting-fnft-utils.ts
@@ -1,14 +1,24 @@
 import { Lucid, MintingPolicy, PolicyId, TxHash, Unit, utf8ToHex, Data, UTxO, Constr, Script } from "lucid-cardano"
 
-interface Options {
+interface BurnOptions {
   lucid: Lucid
   address: string
-  name: string
+  unitNFT: string
+  mintingPolicy: MintingPolicy,
+  redeemerBurnFNFT: string,
+  unitFNFT: string,
+  utxo: UTxO,
+  fnftScript: Script
+}
+
+interface MintOptions {
+  lucid: Lucid
+  address: string
+  unitNFT: string
   mintingPolicy: MintingPolicy,
   redeemerMintFNFT: string,
   tokenFNFTName: string,
-  utxos: UTxO[],
-  fnftScript: Script
+  utxo: UTxO
 }
 
 // fully qualified asset name, hex encoded policy id + name
@@ -31,20 +41,21 @@ const getPolicyId = (lucid: Lucid, mintingPolicy: MintingPolicy) => {
   return policyId
 }
 
-export const mintFNFT = async ({ lucid, address, name, mintingPolicy, redeemerMintFNFT, tokenFNFTName, utxos, fnftScript }: Options): Promise<TxHash> => {
+export const mintFNFT = async ({ lucid, address, unitNFT, mintingPolicy, redeemerMintFNFT, tokenFNFTName, utxo, }: MintOptions): Promise<TxHash> => {
   const policyId = getPolicyId(lucid, mintingPolicy)
   const unitFNFT =  policyId + tokenFNFTName
+  console.log({unitFNFT})
   const unitValidation = getUnit(policyId, 'FNFT_VALIDITY')
-  const unitNFT = getUnit("37ab27a5bdceb3b82583b66788baa912e520c72f6df13318b22f6942","nft1")
+  
   console.log(tokenFNFTName)
 
   const datum = Data.to(
-    new Constr(0, [ policyId,tokenFNFTName, 1000n, "37ab27a5bdceb3b82583b66788baa912e520c72f6df13318b22f6942", utf8ToHex("nft1"), 1000n])
+    new Constr(0, [ policyId,tokenFNFTName, 1000n, unitNFT.substring(0,56), unitNFT.substring(56), 1000n])
   );
 
   const tx = await lucid
     .newTx()
-    .readFrom([utxos[1]])
+    .readFrom([utxo])
     .mintAssets({ [unitFNFT]: 1000n, [unitValidation]: 1n}, redeemerMintFNFT)
     .attachMintingPolicy(mintingPolicy)
     .payToContract(address, {inline: datum} ,{ [unitFNFT]: 1000n, [unitValidation]: 1n, [unitNFT]: 1n})
@@ -55,27 +66,20 @@ export const mintFNFT = async ({ lucid, address, name, mintingPolicy, redeemerMi
   const txHash = await signedTx.submit()
 
   return txHash
+  // fnftunit "ed0ac7e51d2ad9b7e0118a98ed574033a6a9cf9079e125ac29e0fc5c44c7d8504f5fb54f5cac442b17f085387bcf51c600bb7aab7a9ddd1f165503bf"
 }
 
-export const burnFNFT = async ({ lucid, address, name, mintingPolicy, redeemerMintFNFT, tokenFNFTName, utxos, fnftScript }: Options): Promise<TxHash> => {
+export const burnFNFT = async ({ lucid, address, unitNFT, mintingPolicy, redeemerBurnFNFT, unitFNFT, utxo, fnftScript }: BurnOptions): Promise<TxHash> => {
   const policyId = getPolicyId(lucid, mintingPolicy)
-  const unit = getUnit(policyId, name)
-
-  const unitFNFT =  "ed0ac7e51d2ad9b7e0118a98ed574033a6a9cf9079e125ac29e0fc5c" + "9e06e052de79ac4e2df08e8076bb97b52bc200e12ff350101abd37c1b66de95c"
+  // const unitFNFT =  "ed0ac7e51d2ad9b7e0118a98ed574033a6a9cf9079e125ac29e0fc5c" + "9e06e052de79ac4e2df08e8076bb97b52bc200e12ff350101abd37c1b66de95c"
   const unitValidation = getUnit(policyId, 'FNFT_VALIDITY')
-  const unitNFT = getUnit("37ab27a5bdceb3b82583b66788baa912e520c72f6df13318b22f6942","nft1")
-  console.log(tokenFNFTName)
-
-  const datum = Data.to(
-    new Constr(0, [ policyId,tokenFNFTName, 1000n, "37ab27a5bdceb3b82583b66788baa912e520c72f6df13318b22f6942", utf8ToHex("nft1"), 1000n])
-  );
   const tx = await lucid
 
   .newTx()
-  .readFrom([utxos[0]])
-  .collectFrom([utxos[0]], redeemerMintFNFT)
+  .readFrom([utxo])
+  .collectFrom([utxo], redeemerBurnFNFT)
   .attachSpendingValidator(fnftScript)
-  .mintAssets({ [unitFNFT]: -1000n, [unitValidation]: -1n}, redeemerMintFNFT)
+  .mintAssets({ [unitFNFT]: -1000n, [unitValidation]: -1n}, redeemerBurnFNFT)
   .attachMintingPolicy(mintingPolicy)
   .payToAddress(address,{[unitNFT]: 1n})
   .complete()
